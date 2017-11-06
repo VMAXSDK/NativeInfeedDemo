7ae2f6c9da6ddbd7ab1145815190f6d15684f0a6
prathameshs <prathamesh.s@vmax.com>
2017-11-06 11:57:07 +0530

    3.7.12 update

--- app/src/main/java/com/vmax/demo/nativeInfeed/MainActivity.java
+++ app/src/main/java/com/vmax/demo/nativeInfeed/MainActivity.java
@@ -2,7 +2,6 @@ package com.vmax.demo.nativeInfeed;
 
 
 import android.content.Context;
-import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
